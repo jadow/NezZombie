7d34c94f3f11fd0f639c4b0b7b9e1e165169a85d
Jadow <kagenoou@gmail.com>
2019-01-30 21:07:56 +0800

    feat: add nez source

---
+++ NezZombie/Scenes/MasterScene.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NezZombie.Scenes
+{
+    class MasterScene: Scene
+    {
+        public override void initialize()
+        {
+            clearColor = Color.LightGray;
+            addRenderer(new DefaultRenderer());
+
+            // var mapLoaderEntity = createEntity("map-loader");
+            var playerEntity = createEntity("player");
+
+            // mapLoaderEntity.addComponent(new MapLoader(playerEntity));
+
+            // var playerTexture = content.Load<Texture2D>("tinyRPG-forest-code/PNG/sprites/hero/idle/hero-idle-back/hero-idle-back");
+            //playerEntity.addComponent(new Sprite(playerTexture));
+            playerEntity.addComponent(new PrototypeSprite(16, 16)).setColor(Color.Red);
+
+            //playerEntity.addComponent(new PlayerMover());
+
+            playerEntity.setPosition(new Vector2(0, 0));
+        }
+    }
+}
